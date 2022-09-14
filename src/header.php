efd2e0d47bd44218dbd14749575e437194882856
Ethan <weitkampe@etown.edu>
2022-09-14 19:44:37 -0400

    Base files added

---
+++ src/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Jays Online Ordering</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel ="stylesheet" href="<?php echo $path; ?>style.css">
+    </head>
+<?PHP
+include "navbar.php";
+?>
--- src/index.php
+++ src/index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jay's Online Ordering</title>
+    <title>Jays Online Ordering</title>
 </head>
 <body>
+
+    <?PHP
+        require('header.php')
+        #require('navbar.php')
+    ?>
     
 </body>
 </html>
