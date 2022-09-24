a6b8db7599738d644dc066f791659a6fb0934289
Ethan <weitkampe@etown.edu>
2022-09-24 15:48:45 -0400

    Added functions for reading menu items from db.

--- src/menu.php
+++ src/menu.php
@@ -1,7 +1,16 @@
+<?php
+    require("menuFunctions.php");
+    require("config.php");
+    require("database_functions.php");
+?>
 
 
 <div id="menu-container">
 
-    <!-- Should probably read from a database to know which menu items to show and their details -->
+    <!-- Reads from a database to know which menu items to show and their details -->
+    <?php
+        displayMenu();
+    ?>
+
 
 </div>
--- src/menuFunctions.php
+++ src/menuFunctions.php
@@ -1,6 +1,23 @@
 <?php
 
-function displayItem($itemName, $price, $description, $calories, $available){
+
+function  displayMenu(){
+    $pdo = connect_to_db();
+    $stmt = $pdo->prepare("SELECT * FROM menu WHERE isAvailable=:available");
+    $stmt->execute([':available' => 1]); 
+    $data = $stmt->fetchall();
+    
+    foreach ($data as $item){
+        displayItem($item['itemName'],$item['price'],$item['category'],$item['description'],$item['calories'],$item['isAvailable']);
+    }
+}
+
+
+function displayItem($itemName, $price, $category, $description, $calories, $available){
+
+    echo '
+    <p>'.$itemName.': '.$price.'  |<p>
+    ';
 
 }
 
