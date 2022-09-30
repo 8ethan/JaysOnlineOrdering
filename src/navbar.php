1bc393fcc65c266f7448c366d89fbd63295bfed0
Nate <natemans01@gmail.com>
2022-09-30 00:50:21 -0400

    Added number to indicate items in order

--- src/navbar.php
+++ src/navbar.php
@@ -1,6 +1,6 @@
 <div class="sidenav">
   <a href="#">Menu</a>
-  <a href="#">Your Order</a>
+  <a href="#" id='yoder'>Your Order<div id='yoderNotif'>5</div></a>
   <a href="#">Stats</a>
   <a href="#">Admin</a>
 </div>
