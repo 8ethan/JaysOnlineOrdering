5179d553983d803d9d78ae06dd22f6d37bd85602
8ethan <weitkampe@etown.edu>
2022-10-11 10:35:51 -0400

    changed some css jaunts

--- src/navbar.php
+++ src/navbar.php
@@ -1,6 +1,6 @@
 <div class="sidenav">
   <a href="index.php" class="" >Menu</a>
-  <div id="yoder"><a href="cart.php" class="">Your Order<div id="yoderNotif">5</div></a></div>
+  <div id="yoder"><a href="cart.php" class="">Your Order<div id="yoderNotif">0</div></a></div>
   <a href="stats.php" class="" >Stats</a>
   <a href="admin.php" class="" >Admin</a>
 </div>
