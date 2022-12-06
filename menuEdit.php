f3c34dc9d8912b26b6e7f0f3ef198cc171783ee7
8ethan <weitkampe@etown.edu>
2022-12-06 15:31:28 -0500

    Added updating current menu items to API.

--- menuEdit.php
+++ menuEdit.php
@@ -3,10 +3,18 @@
 require("functions/database_functions.php");
 require("functions/config.php");
 
-if (isset($_POST['itemName']) && isset($_POST['category']) && isset($_POST['price']) && isset($_POST['desc']) 
-&& isset($_POST['calories']) && isset($_POST['isAvailable'] && isset($_POST['dir']))){
+# Post parameters for a new item
+$all_params = isset($_POST['itemName']) && isset($_POST['category']) && isset($_POST['price']) && isset($_POST['desc']) 
+&& isset($_POST['calories']) && isset($_POST['isAvailable']) && isset($_POST['dir']); 
+
+if (!isset($_POST['editID']) && $all_params){
     createItem();
 }
+# Post parameters for editing an item
+else if (isset($_POST['editID']) && $all_params){
+    updateItem();
+}
+# Post parameter for deleting an item
 else if(isset($_POST['deleteID'])){
     deleteItem();
 } else {
@@ -20,6 +28,17 @@ function createItem(){
     $stmt = $db->prepare($sql);
     $stmt->execute(array('item_name'=>$_POST['itemName'], 'cat'=>$_POST['category'], 'price'=>$_POST['price'],
     'desc'=>$_POST['desc'], 'cal'=>$_POST['calories'], 'isAvail'=>$_POST['isAvailable'], 'dir'=>$_POST['dir']));
+}
+
+function updateItem(){
+    $db = connect_to_db();
+    $sql = "UPDATE menu SET itemName=:item_name, category=:cat,
+    price=:price, description=:desc, calories=:cal, isAvailable=:isAvail, imgDir=:dir
+    WHERE itemID=:item_id";
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array('item_name'=>$_POST['itemName'], 'cat'=>$_POST['category'], 'price'=>$_POST['price'],
+    'desc'=>$_POST['desc'], 'cal'=>$_POST['calories'], 'isAvail'=>$_POST['isAvailable'], 'dir'=>$_POST['dir']),
+    'item_id'=>$_POST['editID']);
 }
 
 function deleteItem(){
