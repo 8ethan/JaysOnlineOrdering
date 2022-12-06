7b2ea0c031f130834ef76832b84fa5d59ebbaecc
8ethan <weitkampe@etown.edu>
2022-12-06 14:53:48 -0500

    Added api for adding and removing menu items from the db

---
+++ menuEdit.php
@@ -0,0 +1,30 @@
+<?php
+
+require("functions/database_functions.php");
+require("functions/config.php");
+
+if (isset($_POST['itemName']) && isset($_POST['category']) && isset($_POST['price']) && isset($_POST['desc']) 
+&& isset($_POST['calories']) && isset($_POST['isAvailable'])){
+    createItem();
+}
+else if(isset($_POST['deleteID'])){
+    deleteItem();
+} else {
+    echo "Please provide the correct POST paramters.";
+}
+
+function createItem(){
+    $db = connect_to_db();
+    $sql = "INSERT INTO menu (itemName, category, price, description, calories, isAvailable) 
+    VALUES (:item_name, :cat, :price, :desc, :cal, :isAvail)";
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array('item_name'=>$_POST['itemName'], 'cat'=>$_POST['category'], 'price'=>$_POST['price'],
+    'desc'=>$_POST['desc'], 'cal'=>$_POST['calories'], , 'isAvail'=>$_POST['isAvailable']));
+}
+
+function deleteRoom(){
+    $db = connect_to_db();
+    $sql = "DELETE FROM menu WHERE itemID = :item_id";
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array('item_id'=>$_POST['deleteID']));
+}
