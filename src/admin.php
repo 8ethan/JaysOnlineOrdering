08f744346fb2227525285a57566f5264f53aee9b
8ethan <weitkampe@etown.edu>
2022-12-08 15:39:19 -0500

    More admin page layout.

--- src/admin.php
+++ src/admin.php
@@ -30,7 +30,30 @@
                 <div id="modPanel">
                     <h2>Edit Item:</h2>
                     <form id="editForm">
-                        <p>Item ID: </p> <label for="itemName">Item Name: </label>
+                        <div id="editFormContainer">
+                            <div>
+                                <p>Item ID: </p>
+                                <label for="itemName">Item Name: </label>
+                                <input type="text" name="itemName"><br>
+                                <label for="category">Category:</label>
+                                <input type="text" name="category"><br>
+                                <label for="price">Price: $</label>
+                                <input type="number" name="price"><br>
+                                <label for="available">Available:</label>
+                                <input type="checkbox" name="available"><br>
+                                <label for="img">New image:</label>
+                                <input type="file" name="img" accept="image/*"><br>
+                                <input type="submit" value="Save Changes">
+                            </div>
+                            <div id="itemImage">
+                                <img id="editImage"><br>
+                            </div>
+                        </div>
+                    </form>
+                    <br>
+                    <h2>Add New Item:</h2>
+                    <form id="addForm">
+                        <label for="itemName">Item Name: </label>
                         <input type="text" name="itemName"><br>
                         <label for="category">Category:</label>
                         <input type="text" name="category"><br>
@@ -38,12 +61,10 @@
                         <input type="number" name="price"><br>
                         <label for="available">Available:</label>
                         <input type="checkbox" name="available"><br>
-                        <p>Current Image:</p>
-                        <img id="editImage" src='https://cdnimg.webstaurantstore.com/images/products/large/520039/1998265.jpg'><br>
-                        <input type="submit" value="Save Changes">
+                        <label for="img">Upload image:</label>
+                        <input type="file" name="img" accept="image/*"><br>
+                        <input type="submit" value="Add Item">
                     </form>
-                    <h2>Add New Item:</h2>
-
                 </div>
             </div>
             <footer>
