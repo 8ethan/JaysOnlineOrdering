2c3f099d20f91b62d907bbc40e9f9492d7facb04
8ethan <weitkampe@etown.edu>
2022-12-08 15:10:32 -0500

    jaunts

--- src/admin.php
+++ src/admin.php
@@ -21,34 +21,33 @@
             </div>
             <div id="editContainer">
                 <table id="editTable">
-                    <tr>
+                    <tr id="editHeader">
                         <th>Item ID</th>
                         <th>Item Name</th>
                         <th>Price</th>
                     </tr>
                 </table>
-                <form id="editForm">
-                    <p>Item ID: </p>
-                    <label for="itemName">Item Name:</label><br>
-                    <input type="text" name="itemName"><br>
-                    <label for="category">Category:</label><br>
-                    <input type="text" name="category"><br>
-                    <label for="price">Price:</label><br>
-                    <input type="number" name="price"><br>
-                    <label for="available">Availability:</label>
-                    <input type="checkbox" name="available"><br>
-                    <p>Current Image:</p>
-                    <img id="editImage" src='https://cdnimg.webstaurantstore.com/images/products/large/520039/1998265.jpg'><br>
-                    <input type="submit" value="Save Changes">
-                </form>
+                <div id="modPanel">
+                    <h2>Edit Item:</h2>
+                    <form id="editForm">
+                        <p>Item ID: </p> <label for="itemName">Item Name: </label>
+                        <input type="text" name="itemName"><br>
+                        <label for="category">Category:</label>
+                        <input type="text" name="category"><br>
+                        <label for="price">Price: $</label>
+                        <input type="number" name="price"><br>
+                        <label for="available">Available:</label>
+                        <input type="checkbox" name="available"><br>
+                        <p>Current Image:</p>
+                        <img id="editImage" src='https://cdnimg.webstaurantstore.com/images/products/large/520039/1998265.jpg'><br>
+                        <input type="submit" value="Save Changes">
+                    </form>
+                    <h2>Add New Item:</h2>
+
+                </div>
             </div>
             <footer>
             </footer>
-            <div id="checkoutButton" class="pushButton">
-                <div id="checkoutBadge">
-                    <span>5</span>
-                </div>
-            </div>
         </main>
 
     </body>
