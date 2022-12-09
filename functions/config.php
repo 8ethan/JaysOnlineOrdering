217a5697a3e75ce5647b5d6152c1868b790c6756
Nate <natemans01@gmail.com>
2022-12-08 21:36:47 -0500

    took quotes out of config

--- functions/config.php
+++ functions/config.php
@@ -1,12 +1,12 @@
 <?php
-/* $databasehost = "getenv('INSTANCE_UNIX_SOCKET')";
-$database = "getenv('CLOUDSQL_DB')";
-$databaseuser = "getenv('CLOUDSQL_USER')";
-$databasepassword = "getenv('CLOUDSQL_PASSWORD')"; */
+$databasehost = getenv('INSTANCE_UNIX_SOCKET');
+$database = getenv('CLOUDSQL_DB');
+$databaseuser = getenv('CLOUDSQL_USER');
+$databasepassword = getenv('CLOUDSQL_PASSWORD');
 
 
-$databasehost = "156.67.74.51";
+/* $databasehost = "156.67.74.51";
 $database = "u413142534_project8";
 $databaseuser = "u413142534_python";
-$databasepassword = "M0delzR@wesome";
+$databasepassword = "M0delzR@wesome"; */
 ?>
